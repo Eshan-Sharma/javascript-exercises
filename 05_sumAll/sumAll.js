29f5f2647a476de163a98614d4583b0bd42b8508
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-10-31 20:29:07 +0530

    modified:sumAll, learnt isInteger usage

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let total = 0;
+    if(!Number.isInteger(numOne) || !Number.isInteger(numTwo)){
+        return "ERROR";
+    }
+    if((numOne<0||numTwo<0)){
+        return "ERROR";
+    }
+    if(numOne>numTwo){
+        let temp = numOne;
+        numOne = numTwo;
+        numTwo = temp;
+    }
+    for(let i = numOne; i <= numTwo; i++){
+        total +=i;
+    }
+    return total;
 };
 
 // Do not edit below this line
