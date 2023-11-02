294ebebbcec027cc5c7b65c2370025bbf3725172
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-11-02 18:33:49 +0530

    modified: added the calculator logic

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arrSum = 0;
+	for(let i = 0; i < array.length; i++){
+    arrSum += array[i];
+  }
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  arrMul = 1;
+  for(let i = 0; i < array.length;i++){
+    arrMul *= array[i];
+  }
+  return arrMul;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num==0 || num==1){
+    return 1;
+  }
+  fact = 1;
+  for(let i = 1; i <= num; i++){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
