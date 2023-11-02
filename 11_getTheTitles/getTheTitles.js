3a0ecac9836da72a12ef7255869ab8aaf4438f38
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-11-02 19:57:25 +0530

    modified: getting keys from dictionary

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(dict) {
+    let newArr = [];
+    for(let i = 0; i < dict.length;i++){
+        newArr.push(dict[i].title)
+    }
+    return newArr;
 };
 
 // Do not edit below this line
