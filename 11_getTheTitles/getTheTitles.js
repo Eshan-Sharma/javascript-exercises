bd70dbbac919b7a1e91e276ac17249bcbcdcb02b
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2023-11-02 20:02:58 +0530

    modified: titles file, used map to get tiles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,10 @@
 const getTheTitles = function(dict) {
-    let newArr = [];
-    for(let i = 0; i < dict.length;i++){
-        newArr.push(dict[i].title)
-    }
-    return newArr;
+    return dict.map( (book)=>book.title);
+    // let newArr = [];
+    // for(let i = 0; i < dict.length;i++){
+    //     newArr.push(dict[i].title)
+    // }
+    // return newArr;
 };
 
 // Do not edit below this line
